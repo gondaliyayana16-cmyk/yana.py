88d6262327dc328657ef1ed4bcd37b8788715e84
gondaliyayana16-cmyk <gondaliyayana16@gmail.com>
2026-01-06 08:03:23 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,30 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello Word")
+Hello Word
+x=3
+print(x)
+3
+y=20.5
+print (y)
+20.5
+x=1j
+print (x)
+1j
+A=["apple","bnana","cherry",]
+print (A)
+['apple', 'bnana', 'cherry']
+B=9"apple","banana","cherry",
+SyntaxError: invalid syntax
+
+B=["apple","bnna","cherry",]
+print (B)
+['apple', 'bnna', 'cherry']
+P=true
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    P=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+P=True
+print (P)
+True
