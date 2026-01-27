e1f24b08139aba8aeb87c3778dca63294fcaac24
gondaliyayana16-cmyk <gondaliyayana16@gmail.com>
2026-01-27 08:12:36 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,75 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Arithmetic operators
+>>> a=5
+>>> b=10
+>>> print(a+b)
+15
+>>> print(a-b)
+-5
+>>> print(a/b)
+0.5
+>>> #Assignment operators
+>>> x=10
+>>> print(x)
+10
+>>> #Unary Minus
+>>> a=5
+>>> print(-a)
+-5
+>>> #Relational operators
+>>> p=10
+>>> q=5
+>>> print(p>q)
+True
+>>> print(p==q)
+False
+>>> #Logical operators
+>>> x=True
+>>> y=False
+>>> print(x&y)
+False
+>>> print(xory)
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    print(xory)
+NameError: name 'xory' is not defined
+>>> print(x or y)
+True
+>>> print(not y)
+True
+>>> #boolean operators
+>>> is_pass
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    is_pass
+NameError: name 'is_pass' is not defined
+>>> is_pass=True
+>>> print(is_pass)
+True
+>>> #Bitwise operators
+>>> a=3
+>>> b=5
+>>> print(a&b)
+1
+>>> print(a/b)
+0.6
+>>> #identity operators
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
+>>> #membership operators
+>>> number=[1234]
+>>> print(2 in numbers)
+Traceback (most recent call last):
+  File "<pyshell#42>", line 1, in <module>
+    print(2 in numbers)
+NameError: name 'numbers' is not defined
+>>> print(2 in number)
+False
+>>> print(5 not in number)
+True
+>>> 
