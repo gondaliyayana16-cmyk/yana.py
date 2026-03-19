93dbfb11eaab91df4111c76c809fb1ff9eae3949
gondaliyayana16-cmyk <gondaliyayana16@gmail.com>
2026-03-19 11:03:45 +0530

    Add files via upload

---
+++ keywordargu.py
@@ -0,0 +1,13 @@
+#basic keyword argument
+def student_info(name,age,city)
+    print("Name:",name) 
+    print("Age:",age)
+    print("city:",city)
+student_info(age=18,city="Rajkot",name="Ravi")          
+# mixing positinal and keyword
+def display(a,b,c):
+    print("a=",a)
+    print("b=",b)
+    print("c=",c)
+display(a=3,b=2,c=1)    
+
